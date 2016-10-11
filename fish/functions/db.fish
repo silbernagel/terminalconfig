06f57b22195e981fc9cf7f9bb6570d9eb57934fb
Jorg Rødsjø <jorg.rodsjo@telenordigital.com>
2016-10-11 18:38:31 +0200

    .

--- fish/functions/db.fish
+++ fish/functions/db.fish
@@ -1,19 +1,19 @@
-set -l myip (ip)
+function db
+         set -l myip (ip)
+         switch $myip   
+         case '180.87.100.10'
+                set shard 'singapore'
+         case '62.92.26.133'
+              set shard 'norway'
+         end    
 
+         if [ $shard = "singapore" ]
+              echo $shard
+              db_sg
+         else if [ $shard = "norway" ]
+              db_no
+         else
+              echo "unknown datacentre"
+         end
 
-switch $myip
-case '180.87.100.10'
-    set shard 'singapore'
-case '62.92.26.133'
-    set shard 'norway'
-end    
-
-if [ $shard = "singapore" ]
-  echo $shard
-  db_sg
-else if [ $shard = "norway" ]
-  db_no
-else
-  echo "unknown datacentre"
 end
-
