aaecf97bf279d6f58ca837f14fcd3823e87bad9a
Jorg Rødsjø <jorg@neoplex.org>
2016-10-15 09:49:06 +0200

    Adding ctrl-e as auto-completion-accept and enter

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,4 @@
-#function fish_user_key_bindings
-#    bind \r 'commandline -f accept-autosuggestion execute'
-#    bind \ck 'commandline -f kill-line suppress-autosuggestion'
-#end
+function fish_user_key_bindings
+    bind \ce 'commandline -f accept-autosuggestion execute'
+    bind \ck 'commandline -f kill-line suppress-autosuggestion'
+end
